3c8c280881638f9e863eabdc8c183f6cee5a8e1d
vivekvaradarajan1980 <varadarajan.vivek@gmail.com>
2022-01-02 16:02:04 -0600

    WIP Add more items using migration file

--- foodTruckProject/src/main/java/com/example/foodTruckProject/FoodController.java
+++ foodTruckProject/src/main/java/com/example/foodTruckProject/FoodController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-
+@CrossOrigin(origins = "http://localhost:3000")
 public class FoodController {
 
     @Autowired
---
+++ foodTruckProject/src/main/resources/db/migration/V4__add_more_item.sql
@@ -0,0 +1,31 @@
+INSERT INTO food_menu(
+    name, description, price
+) values (
+             'WARM APPLE CRUMBLE', 'vanilla bean ice cream', 4.78
+         );
+
+
+INSERT INTO food_menu(
+    name, description, price
+) values (
+             'SMOKED AND GRILLED PORK BELLY', 
+              'bacon-onion marmalade • apple slaw', 9.78
+         );
+
+
+INSERT INTO food_menu(
+    name, description, price
+) values (
+             'BACON-WRAPPED SHRIMP', 
+              'jalapeno-cilantro slaw, chipotle mayo
+', 4.78
+         );
+   
+
+INSERT INTO food_menu(
+    name, description, price
+) values (
+             'SMOKED BEEF BRISKET POUTINE', 
+              'crispy fries • beef brisket gravy • chopped brisket • white cheddar cheese curds • classic bbq sauce • chives •', 8.78
+         );
+   
