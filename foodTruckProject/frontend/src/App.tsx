3c696edf8d31fbed4c425a50a66cd846c09016d4
Gregory Brothers <gregory.d.brothers.mil@mail.mil>
2022-01-08 10:18:35 -0600

    add app bar

--- foodTruckProject/frontend/src/App.tsx
+++ foodTruckProject/frontend/src/App.tsx
@@ -1,17 +1,16 @@
+import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
+import Menu from "@material-ui/icons/Menu";
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import {FoodItem} from "./FoodItem";
 
-type Props = {
-    foodItems: FoodItem[];
-};
+
+
 function App() {
     const [foodList, setFoodList] = useState<FoodItem[] >([]);
     const [showDescription, setShowDescription] = useState<boolean>(false);
-
-
-
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
         
@@ -27,19 +26,32 @@ function App() {
     const getDescriptionOfFoodItem = () =>{
         setShowDescription(!showDescription)
     }
+
+    const handleSearchBox = (event: React.ChangeEvent<HTMLInputElement>) =>{
+        setSearch(event.target.value)
+    }
     // @ts-ignore
     return (
         <div className="App">
+            <AppBar position="sticky" style={{backgroundColor: "darkolivegreen", color: "black"}}>
+                <Toolbar>
+                    <IconButton aria-label="app">
+                        <Menu />
+                    </IconButton>
+                    <Typography variant="h6"> V & G Food Truck </Typography>
+                </Toolbar>
+            </AppBar>
             <h1 >V & G Food Truck</h1>
-            <input type="text" placeholder="search..."/>
+            <input onChange={(event => handleSearchBox(event))} type="text" placeholder="search for food..."/>
             <ul className='foodList' data-testid="menu-items" >
-                {foodList.map((each, index) => (
+                {foodList.filter(each => each.name.toLowerCase().includes(search.toLowerCase()) ||
+                    each.description.toLowerCase().includes(search.toLowerCase())).map((each, index) => (
                     <>
                         <li key={index}>
-                            <div  key={index}>{each.name} {each.price}<br></br></div>
+                            <div  key={index}>{each.name} {each.price}<br/></div>
                             {showDescription && <div  key={index}><b>{each.description}</b></div>}
                         </li>
-                        <br></br>
+                        <br/>
                        
                     </>))}
             </ul>
@@ -48,6 +60,5 @@ function App() {
 
     );
 }
-//<Button variant="text">Text</Button>
 
 export default App;
